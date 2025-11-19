ed78b932825d850ea74723723d569a1500bd9d9b
Arthur Milani Giovanini <arthur.milani@totvs.com.br>
2025-11-19 16:31:32 -0300

    Adding CORS permission and fixing responses status code errors

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import UploadFile, File, Form
 from pathlib import Path
 from fastapi import HTTPException
@@ -9,6 +10,20 @@ EXTRACT_DIR.mkdir(exist_ok=True)
 
 app = FastAPI()
 
+ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+# CORS configuration: it's necessary to allow sending responses to the front end
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],
+)
+
 @app.get("/simulations")
 def get_simulations():
     return {"message": "Hello, World!"}
@@ -16,6 +31,7 @@ def get_simulations():
 @app.get("/simulations/{simulation_id}")
 def get_simulation_by_id(simulation_id: int):
     return {"simulation_id": simulation_id, "status": "running"}
+
 
 #Receive .zip file and extract contents
 @app.post("/simulations/upload")
@@ -25,17 +41,19 @@ async def upload_simulation_file(
     sim_type: str = Form(...)
     ):
     
+    # Validate file type and simulation type
     if not file.filename.endswith('.zip'):
-        return {"error": "Only .zip files are accepted", "status_code": 400}
+        raise HTTPException(status_code=400, detail="Only .zip files are accepted")
     elif sim_type not in ["default", "optimized"]:
-        return {"error": "Invalid simulation type", "status_code": 400}
+        raise HTTPException(status_code=400, detail="Invalid simulation type")
     
     
     zip_content = await file.read()
 
+    # Try to extract the zip file
     try:
         with zipfile.ZipFile(io.BytesIO(zip_content)) as zip_file:
-            dest = EXTRACT_DIR / file.filename.replace(".zip", "")
+            dest = EXTRACT_DIR / name
             zip_file.extractall(dest)
             extracted_files = zip_file.namelist()
 
@@ -44,6 +62,7 @@ async def upload_simulation_file(
     
     # TODO: Save data into database
     
+    #Call the simulation script based on sim_type
     if sim_type == "default":
         print("Processing default simulation...")
         # subprocess.Popen(["python3", "count.py"])
@@ -53,9 +72,3 @@ async def upload_simulation_file(
         #TODO: Call the optimized simulation script here
 
     return {"files_extracted": extracted_files, "status_code": 200}
-
-
-
-#TODO: A função só retorna 200, mesmo em caso de erro.
-#TODO: O front end acusa erro em qualquer situação.
-#TODO: Quando envio um arquivo e limpo no front end, preciso recarregar para carregar outro arquivo
